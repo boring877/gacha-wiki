4bbf6b004f4e21a7e30b9812cc634abb67f65254
Boring877 <elfhuyace@gmail.com>
2025-05-30 23:01:27 +0000

    Release v2.0.1: Error fixes, removed MDX integration, fixed favicon, cleaned dependencies, resolved file permissions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from '@unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), UnoCSS()],
+	integrations: [sitemap(), UnoCSS()],
 });
