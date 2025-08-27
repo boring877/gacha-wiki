3134b34659c2e85270ca76a42ba692b836ea73c2
Boring877 <elfhuyace@gmail.com>
2025-08-27 11:06:44 +0000

    Remove test file

--- test-prettier.js
+++
@@ -1,9 +0,0 @@
-// Test file to verify Prettier formatting
-const _testFunction = () => {
-  const _message = 'testing prettier formatting';
-  return {
-    success: true,
-    message: _message,
-    data: [1, 2, 3, 4, 5],
-  };
-};
