e0b09a6de913061e1ca5ea276a371cfab372ac39
Boring877 <elfhuyace@gmail.com>
2025-09-30 05:19:44 +0000

    chnage from vercel to replit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gachawiki.info',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
@@ -19,8 +14,6 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-  // Static site with Vercel API routes
-  output: 'static',
   // Image optimization
   image: {
     service: {
