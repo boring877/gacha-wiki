2caed7bca27363e7ddd19ada84bab9cec1fab5de
boring877 <elfhuyace@gmail.com>
2026-04-16 02:24:17 +0000

    TS: add redeem codes page with TAIMANINTHX coupon (800 Gems)

---
+++ src/data/taimanin-squad/redeem-codes.js
@@ -0,0 +1,14 @@
+/**
+ * Taimanin Squad - Redeem Codes Data
+ */
+
+export const redeemCodes = [
+  {
+    code: 'TAIMANINTHX',
+    source: 'Collaboration Event Appreciation',
+    validUntil: '2026-05-08 14:59 UTC',
+    rewards: '800 Gems',
+    status: 'active',
+    notes: 'Can also be redeemed in Action Taimanin. Register at https://taimaninsquad.com/coupon',
+  },
+];
