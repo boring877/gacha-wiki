77235c3b144f2061985c5e9b0de9e2d4b63d522a
boring877 <elfhuyace@gmail.com>
2026-09-08 05:17:41 +0000

    Horizon Walker: character Story Gallery sections - 99 story CGs across 33 characters from game data

---
+++ src/data/horizon-walker/story-cg.js
@@ -0,0 +1,470 @@
+// Horizon Walker character story CGs (Dialogue_BG_Illustration assets from the game)
+// Keyed by game hero id. NonAdult duplicates and popup crops excluded.
+export const STORY_CG = {
+  "67": [
+    {
+      "file": "story-cg/Ahram/Ahram_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Ahram/Ahram_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Ahram/Ahram_Friendship_002_1.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "44": [
+    {
+      "file": "story-cg/Berga/Berga_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Berga/Berga_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "45": [
+    {
+      "file": "story-cg/Cora/Cora_001.webp",
+      "caption": "Story CG: Cora 001"
+    },
+    {
+      "file": "story-cg/Cora/Cora_002.webp",
+      "caption": "Story CG: Cora 002"
+    }
+  ],
+  "61": [
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_001_LongHair.webp",
+      "caption": "Friendship Story CG 1 (long hair variant)"
+    },
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_002_LongHair.webp",
+      "caption": "Friendship Story CG 2 (long hair variant)"
+    },
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    },
+    {
+      "file": "story-cg/Echidna/Echidna_Friendship_004.webp",
+      "caption": "Friendship Story CG 4"
+    }
+  ],
+  "27": [
+    {
+      "file": "story-cg/Efreeti/Efreeti_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Efreeti/Efreeti_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Efreeti/Efreeti_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    },
+    {
+      "file": "story-cg/Efreeti/Efreeti_Friendship_004.webp",
+      "caption": "Friendship Story CG 4"
+    }
+  ],
+  "59": [
+    {
+      "file": "story-cg/Emilia/Emilia_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Emilia/Emilia_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Emilia/Emilia_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "62": [
+    {
+      "file": "story-cg/Eugenia/Eugenia_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Eugenia/Eugenia_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Eugenia/Eugenia_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "3": [
+    {
+      "file": "story-cg/Everette/WinterEvent_Everette_001.webp",
+      "caption": "Story CG: WinterEvent Everette 001"
+    },
+    {
+      "file": "story-cg/Everette/WinterEvent_Everette_002.webp",
+      "caption": "Story CG: WinterEvent Everette 002"
+    }
+  ],
+  "51": [
+    {
+      "file": "story-cg/Headless_Knight/Halloween_Standing_CutThroatKnight_001.webp",
+      "caption": "Story CG: Halloween Standing CutThroatKnight 001"
+    }
+  ],
+  "37": [
+    {
+      "file": "story-cg/Ines/Ines_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Ines/Ines_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "33": [
+    {
+      "file": "story-cg/Juha/Wasteland_AbandonedCar_Juha_PuppyShooter.webp",
+      "caption": "Story CG: Wasteland AbandonedCar Juha PuppyShooter"
+    }
+  ],
+  "68": [
+    {
+      "file": "story-cg/Juza/Juza_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Juza/Juza_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Juza/Juza_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "55": [
+    {
+      "file": "story-cg/Kalma/Kalma_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Kalma/Kalma_Friendship_001_1.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Kalma/Kalma_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "58": [
+    {
+      "file": "story-cg/Kaul/kaul_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Kaul/kaul_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Kaul/kaul_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    },
+    {
+      "file": "story-cg/Kaul/kaul_Friendship_004.webp",
+      "caption": "Friendship Story CG 4"
+    },
+    {
+      "file": "story-cg/Kaul/kaul_Friendship_005.webp",
+      "caption": "Friendship Story CG 5"
+    }
+  ],
+  "30": [
+    {
+      "file": "story-cg/Kim_Ga-young/KimGayeong_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Kim_Ga-young/KimGayeong_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Kim_Ga-young/KimGayeong_Friendship_002_LongHair.webp",
+      "caption": "Friendship Story CG 2 (long hair variant)"
+    },
+    {
+      "file": "story-cg/Kim_Ga-young/KimGayeong_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "56": [
+    {
+      "file": "story-cg/Korabi/Korabi_001.webp",
+      "caption": "Story CG: Korabi 001"
+    },
+    {
+      "file": "story-cg/Korabi/Korabi_002.webp",
+      "caption": "Story CG: Korabi 002"
+    }
+  ],
+  "69": [
+    {
+      "file": "story-cg/Kotoha/kotoha_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Kotoha/kotoha_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "57": [
+    {
+      "file": "story-cg/Leah/Friendship_Leah_001.webp",
+      "caption": "Story CG: Friendship Leah 001"
+    },
+    {
+      "file": "story-cg/Leah/Friendship_Leah_001_LongHair.webp",
+      "caption": "Story CG: Friendship Leah 001 LongHair"
+    },
+    {
+      "file": "story-cg/Leah/Friendship_Leah_002.webp",
+      "caption": "Story CG: Friendship Leah 002"
+    },
+    {
+      "file": "story-cg/Leah/Friendship_Leah_003.webp",
+      "caption": "Story CG: Friendship Leah 003"
+    },
+    {
+      "file": "story-cg/Leah/MoonCatPart2_Leah_001.webp",
+      "caption": "Story CG: MoonCatPart2 Leah 001"
+    },
+    {
+      "file": "story-cg/Leah/MoonCatPart2_Leah_002.webp",
+      "caption": "Story CG: MoonCatPart2 Leah 002"
+    },
+    {
+      "file": "story-cg/Leah/MoonCatPart2_Leah_003.webp",
+      "caption": "Story CG: MoonCatPart2 Leah 003"
+    },
+    {
+      "file": "story-cg/Leah/MoonCat_Leah_001.webp",
+      "caption": "Story CG: MoonCat Leah 001"
+    },
+    {
+      "file": "story-cg/Leah/MoonCat_Leah_002.webp",
+      "caption": "Story CG: MoonCat Leah 002"
+    }
+  ],
+  "20": [
+    {
+      "file": "story-cg/Lisandria/Lisandria_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Lisandria/Lisandria_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "19": [
+    {
+      "file": "story-cg/Luise/Luise_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Luise/Luise_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Luise/Luise_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "64": [
+    {
+      "file": "story-cg/Marika/Marika_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    }
+  ],
+  "63": [
+    {
+      "file": "story-cg/Maximilia/Maximilia_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Maximilia/Maximilia_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Maximilia/Maximilia_Friendship_002_LongHair.webp",
+      "caption": "Friendship Story CG 2 (long hair variant)"
+    }
+  ],
+  "65": [
+    {
+      "file": "story-cg/Mira/Mira_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Mira/Mira_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "66": [
+    {
+      "file": "story-cg/Mirana/Mirana_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    }
+  ],
+  "25": [
+    {
+      "file": "story-cg/Nari/Campfire_Nari_001.webp",
+      "caption": "Story CG: Campfire Nari 001"
+    },
+    {
+      "file": "story-cg/Nari/Campfire_Nari_002.webp",
+      "caption": "Story CG: Campfire Nari 002"
+    },
+    {
+      "file": "story-cg/Nari/Campfire_Nari_003.webp",
+      "caption": "Story CG: Campfire Nari 003"
+    },
+    {
+      "file": "story-cg/Nari/Campfire_Nari_004.webp",
+      "caption": "Story CG: Campfire Nari 004"
+    },
+    {
+      "file": "story-cg/Nari/Campfire_Nari_005.webp",
+      "caption": "Story CG: Campfire Nari 005"
+    },
+    {
+      "file": "story-cg/Nari/Campfire_Nari_006.webp",
+      "caption": "Story CG: Campfire Nari 006"
+    }
+  ],
+  "39": [
+    {
+      "file": "story-cg/Nika/WinterEvent_MarhimAndNika_001.webp",
+      "caption": "Story CG: WinterEvent MarhimAndNika 001"
+    },
+    {
+      "file": "story-cg/Nika/WinterEvent_MarhimAndNika_001_LongHair.webp",
+      "caption": "Story CG: WinterEvent MarhimAndNika 001 LongHair"
+    }
+  ],
+  "40": [
+    {
+      "file": "story-cg/Nonoha/Nonoha_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Nonoha/Nonoha_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "46": [
+    {
+      "file": "story-cg/Nymset/Nymset_Friendship_002-1.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Nymset/Nymset_Friendship_002-2.webp",
+      "caption": "Friendship Story CG 2"
+    },
+    {
+      "file": "story-cg/Nymset/Nymset_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    }
+  ],
+  "5": [
+    {
+      "file": "story-cg/Olivia/Olivia_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    }
+  ],
+  "60": [
+    {
+      "file": "story-cg/Palekar/Palekar_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Palekar/Palekar_Friendship_001_LongHair.webp",
+      "caption": "Friendship Story CG 1 (long hair variant)"
+    },
+    {
+      "file": "story-cg/Palekar/Palekar_Friendship_003.webp",
+      "caption": "Friendship Story CG 3"
+    },
+    {
+      "file": "story-cg/Palekar/Palekar_Friendship_003_LongHair.webp",
+      "caption": "Friendship Story CG 3 (long hair variant)"
+    }
+  ],
+  "16": [
+    {
+      "file": "story-cg/Pantheri/Pantheri_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Pantheri/Pantheri_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "47": [
+    {
+      "file": "story-cg/Vlissing/Vlissing_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Vlissing/Vlissing_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ],
+  "53": [
+    {
+      "file": "story-cg/Yui/Yui_001.webp",
+      "caption": "Story CG: Yui 001"
+    },
+    {
+      "file": "story-cg/Yui/Yui_002.webp",
+      "caption": "Story CG: Yui 002"
+    },
+    {
+      "file": "story-cg/Yui/Yui_003.webp",
+      "caption": "Story CG: Yui 003"
+    },
+    {
+      "file": "story-cg/Yui/Yui_004.webp",
+      "caption": "Story CG: Yui 004"
+    },
+    {
+      "file": "story-cg/Yui/Yui_005.webp",
+      "caption": "Story CG: Yui 005"
+    },
+    {
+      "file": "story-cg/Yui/Yui_Chocolate_001.webp",
+      "caption": "Story CG: Yui Chocolate 001"
+    },
+    {
+      "file": "story-cg/Yui/Yui_Friendship_001.webp",
+      "caption": "Friendship Story CG 1"
+    },
+    {
+      "file": "story-cg/Yui/Yui_Friendship_002.webp",
+      "caption": "Friendship Story CG 2"
+    }
+  ]
+};
+
+export function getStoryCgByHeroId(heroId) {
+  return STORY_CG[String(heroId)] || null;
+}
